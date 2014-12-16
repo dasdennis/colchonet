4b8ef01dffd2e2df6b22c52fd1d6ddf4f5cc9a9b
Dennis Senner <dennis.as@live.com>
2014-12-16 01:18:27 -0200

    9.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
-Rails.application.routes.draw do
-  scope ":locale" do
+Colchonet::Application.routes.draw do
+  LOCALES = /en|pt\-BR/
+
+  scope ":locale", :LOCALES => LOCALES do
     resources :rooms
     resources :users
 
     resources :user_confirmation, :only => [:show]
   end
 
+  match '/:locale' => 'home#index', :locale => LOCALES
   root 'home#index'
 end

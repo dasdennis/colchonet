1ccfcc4f6120ff2d6ad1ee13e0e05af46ed76b42
Dennis Senner <dennis.as@live.com>
2014-12-18 00:44:16 -0200

    Pag. 183, Cap 9.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user,
-        :notice => 'Cadastro realizado com sucesso!'
+      SignupMailer.confirm_email(@user).deliver
+      redirect_to @user, :notice => 'Cadastro criado com sucesso!'
     else
       render action: :new
     end
